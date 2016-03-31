523d8f45c9ac7e02f463226b518fab5238ecbb82
lilian <lilian.promenzio@gmail.com>
2016-03-31 01:05:30 -0300

    removidas opções para responsibilidade.

--- www/js/slider.js
+++ www/js/slider.js
@@ -1,36 +1,12 @@
 var setSlider = function(sliderID){
 	
 	$('#' + sliderID).lightSlider({
-		item: 4,
 		autoWidth: true,
 		keyPress: true,
 		loop: true,
-		slideMove: 2,
+		slideMove: 1,
 		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
 		speed: 600,
-		responsive : [
-			{
-				breakpoint: 992,
-				settings: {
-					item: 3,
-					slideMove: 1,
-					}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					item: 2,
-					slideMove: 1,
-					}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					item: 1,
-					slideMove: 1
-					}
-			}
-		],
 		
         onSliderLoad: function() {
             $('#' + sliderID).removeClass('cs-hidden');
